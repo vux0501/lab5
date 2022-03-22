e9ee1feeaa5f29fd42e05fb4c95a6c2410e4ee85
Vu Nguyen <hvudtkt@gmail.com>
2022-03-22 14:43:16 +0700

    xong cau 2

---
+++ app/src/main/java/com/example/donut_listview/MainActivityBegin.java
@@ -0,0 +1,26 @@
+package com.example.donut_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivityBegin extends AppCompatActivity {
+    private Button btnStart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_begin);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivityBegin.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
